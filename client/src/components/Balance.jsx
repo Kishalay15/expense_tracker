70198290a20e708bd4cf31760a5f91539d2c8c62
Kishalay15 <kishalaylahiri@gmail.com>
2025-02-05 19:36:38 +0530

    initial commit

---
+++ client/src/components/Balance.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Balance() {
+    return (
+        <>
+            <h3>Balance :</h3>
+            <h2 id='balance'>₹0</h2>
+        </>
+    )
+}
+
+export default Balance
