4860d3bad942b5dedc2bb7e614b051d289c99da7
Kishalay15 <kishalaylahiri@gmail.com>
2025-02-06 22:53:44 +0530

    final commit

--- server.js
+++ server.js
@@ -23,9 +23,18 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api/v1/transactions', transactions)
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/dist'))
+    // app.use(express.static('client/dist'))
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html')))
+    // app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html')))
+
+    const __dirname = path.resolve()
+    app.use(express.static(path.join(__dirname, 'client/dist')))
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+    )
+} else {
+    app.get('/', (req, res) => res.send('Server Ready'.green.bold))
 }
 
 
