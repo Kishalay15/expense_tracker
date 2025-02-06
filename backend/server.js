b8da43dcbf010d1f33325274620366de49828a53
Kishalay15 <kishalaylahiri@gmail.com>
2025-02-07 01:50:05 +0530

    updates

--- backend/server.js
+++ backend/server.js
@@ -33,8 +33,6 @@ if (process.env.NODE_ENV === 'production') {
     app.get('*', (req, res) =>
         res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
     )
-} else {
-    app.get('/', (req, res) => res.send('Server Ready'.green.bold))
 }
 
 
