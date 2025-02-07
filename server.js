f6b67debb37592e5571b713f4e2364df792f75e2
Kishalay15 <kishalaylahiri@gmail.com>
2025-02-07 13:56:44 +0530

    final commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const express = require('express')
+const dotenv = require('dotenv')
+const colors = require('colors')
+const morgan = require('morgan')
+const connectDB = require('./config/db')
+
+dotenv.config({ path: './config/config.env' })
+const PORT = process.env.PORT || 5000
+
+connectDB()
+
+const transactions = require('./routes/transactions')
+
+const app = express()
+
+app.use(express.json())
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+app.use('/api/v1/transactions', transactions)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/dist'))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html')))
+}
+
+
+app.listen(PORT, console.log(`Running`.blue.bold))
